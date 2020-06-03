bd11f983bf0b08f6e0e5f120bdcc0c41d9d12d19
vikasiut <47720978+vikasiut@users.noreply.github.com>
2020-06-03 09:32:33 +0530

    Create tokenization.py

---
+++ tokenization.py
@@ -0,0 +1,8 @@
+"Tokenization : One of the steps of NLP in which text is getting splits into words, symbols, punctuations, spaces and others elements too."
+
+Import spacy
+nlp = spacy.load(‘en’)
+Text = “Ram is going to london”
+doc = nlp(Text)
+tokens = [token.text for token in docx]
+print(Tokens)
