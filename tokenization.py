78b309a4bc8eae97401a5c3825f743057138192f
vikasiut <47720978+vikasiut@users.noreply.github.com>
2020-06-03 09:36:13 +0530

    Update tokenization.py

--- tokenization.py
+++ tokenization.py
@@ -6,3 +6,14 @@ Text = "Ram is going to london"
 doc = nlp(Text)
 tokens = [token.text for token in docx]
 print(Tokens)
+
+#OR
+import spacy
+nlp = spacy.load(‘en’)
+text = “Ram is going to london”
+docx = nlp(text)
+tokens =[]
+for token in docx:
+    tokens.append(token.text) 
+print(Tokens)
+
